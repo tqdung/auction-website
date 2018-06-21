c17c590211ad071cf8a3844ac5555d73bf442b37
Tran Quoc Dung <tqdungit97@gmail.com>
2018-06-21 11:04:38 +0700

    đang làm upload ảnh cho product

--- client_src/assets/me/sell.js
+++ client_src/assets/me/sell.js
@@ -1,34 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var UserInfo = JSON.parse(localStorage.getItem('UserInfo'));
-    if(UserInfo){
+    if (UserInfo) {
         $("#username").text(UserInfo.user_name);
         $(".not_login").hide();
         $(".login_ok").show();
     }
-    else{
+    else {
         $(".login_ok").css("display", "none");
         $(".not_login").show();
     }
 });
 
-// $('#target').submit(function(){
-//     $(this).ajaxSubmit({
-//         beforeSubmit: function(formData, jqForm, options) {
-//             var body = {
-//                 text: "ahihihi"
-//             }
-//             $.ajax({
-//                 url: 'http://localhost:3000/products/test',
-//                 dataType: 'json',
-//                 timeout: 10000,
-//                 type: 'POST',
-//                 contentType: 'application/json',
-//                 data: JSON.stringify(body)
-//             })
-//         },
-//         success: function showResponse(responseText, statusText, xhr, $form) {
-//             alert(responseText);
-//         }
-//     });
-//     return false;
-// });
+
+function getBase64(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        return reader.result;
+    }
+}
+$("#btnSell").on('click', function () {
+    var UserInfo = JSON.parse(localStorage.getItem('UserInfo'));
+    if (UserInfo) {
+        var price_start = $("#price_start").val();
+        var decription = $("#decription").val();
+        var product_name = $("#product_name").val();
+        var price_buying_now = $("#price_buying_now").val();
+        var time_sell = $("#time_sell").datepicker({ dateFormat: "yy-mm-dd" }).val()
+        var price_step = $("#price_step").val();
+        // Get IMAGES
+        var files = document.getElementById('img_product').files;
+        image = files[0];
+        image1 = files[1];
+        image2 = files[2];
+        // var reader = new FileReader();
+        // reader.readAsDataURL(image2);
+        var body = {
+            starting_price: price_start,
+            description: decription,
+            name: product_name,
+            price_buy_now: price_buying_now,
+            time: time_sell,
+            step: price_step,
+            seller: UserInfo.user_id,
+            images: files
+        }
+        $.ajax({
+            url: 'http://localhost:3000/products',
+            dataType: 'json',
+            timeout: 10000,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(body)
+        }).done(function (data) {
+            console.log(data)
+        }).fail(function (xhr, textStatus, error) {
+            console.log(error);
+        })
+    }
+});
